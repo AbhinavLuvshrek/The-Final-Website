b1e3be42843e4dde51499513600f507c4e419c59
AbhinavLuvshrek <113375203+AbhinavLuvshrek@users.noreply.github.com>
2022-11-23 11:23:35 -0700

    111213132123123

---
+++ java/main.js
@@ -0,0 +1,33 @@
+// JavaScript Code
+document.getElementById("btn").addEventListener("click", btnclicked);
+let score = 0;
+
+function btnclicked() {
+  let q1 = document.getElementById("q1").value;
+  let q2 = document.getElementById("q2").value;
+  let q3 = document.getElementById("q3").value;
+  let q4 = document.getElementById("q4").value;
+  let q5 = document.getElementById("q5").value;
+
+  if (q1 === "james naismith") {
+    score++;
+  }
+
+  if (q2 === "springfield") {
+    score++;
+  }
+
+  if (q3 === "aba and nba" || q3 === "nba and aba") {
+    score++;
+  }
+
+  if (q4 === "curry" || q4 === "stephen curry") {
+    score++;
+  }
+
+  if (q5 === "euro league" || q5 === "euroleague") {
+    score++;
+  }
+
+  document.getElementById("output").innerHTML = (score / 5) * 100;
+}
